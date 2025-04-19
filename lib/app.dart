6fd8aa33cb7a8322551ebfea7a327387507316c8
YaelDuarte <yaelduarte48@aragon.unam.mx>
2025-04-18 19:43:54 -0600

    App

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Hola',
+      title: 'Flutter appFrankie',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
